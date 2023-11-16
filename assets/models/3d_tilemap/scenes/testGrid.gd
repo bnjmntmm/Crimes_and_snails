d064a2479e8a6d0c2618541c6268c36179d7191f
timmno12 <benjamintimm21@gmail.com>
2023-11-16 14:29:10 +0100

    Noise -> River
    Somehow made a Noise Dings that makes rivers

--- assets/models/3d_tilemap/scenes/testGrid.gd
+++ assets/models/3d_tilemap/scenes/testGrid.gd
@@ -1,31 +1,27 @@
 extends Node3D
 
 @onready var grid_map = $GridMap
+var waterDip = FastNoiseLite.new()
 var gridTopLayer = []
 @export var noise_active = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if noise_active:
-		var dist = (grid_map.get_used_cells().max()).x + (abs(grid_map.get_used_cells().min())).x
-		var texture = NoiseTexture2D.new()
-		texture.height = dist
-		texture.width = dist
-		texture.noise = FastNoiseLite.new()
-		texture.noise.seed= randi()%10+1
-		await texture.changed
-		var image = texture.get_image()
-		var data = image.get_data()
-		var norm_data = normalize_array(data)
+		randomize()
+		waterDip.seed = randi()
+		waterDip.frequency = 0.005
 		
+		var dist = (grid_map.get_used_cells().max()).x + (abs(grid_map.get_used_cells().min())).x
 		var used_cells = grid_map.get_used_cells()
 		for i in range(used_cells.size()):
 			if used_cells[i].y == 1:
 				gridTopLayer.append(used_cells[i])
+				
 		for i in range(gridTopLayer.size()):
-			var x = gridTopLayer[i].x
-			var z = gridTopLayer[i].z
-			if norm_data[i] < 0.3:
-				grid_map.set_cell_item(Vector3(x,1,z),-1)		
+			var cell = gridTopLayer[i]
+			var waterNoiseAtPos = abs(waterDip.get_noise_2d(cell.x, cell.z))*10
+			if waterNoiseAtPos < 0.3:
+				grid_map.set_cell_item(Vector3i(cell),-1)
 		
 	pass # Replace with function body.
 
