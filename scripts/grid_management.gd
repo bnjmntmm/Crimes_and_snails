506d8544c268e8101291b8c24ad5edce986c970d
Benjamin Timm <benjamintimm21@gmail.com>
2023-12-10 14:20:09 +0100

    Area only placeable if it has area next to itself
    Implemented:
    Area Placement correctly
    Not placeable in mainland
    
    ToDo: random generation of noise for better bumps or so?

--- scripts/grid_management.gd
+++ scripts/grid_management.gd
@@ -1,13 +1,30 @@
 extends Node3D
 @export var Grid_scenes: Array[PackedScene] 
-
+var mesh_lib_src : MeshLibrary= preload("res://assets/grid_blocks/tileMap.tres")
 # Called when the node enters the scene tree for the first time.
+
+var chunk_size = 64
+var preGenSize = 128
 func _ready():
-	randomize()
-	var randomGridInt = randi_range(0, Grid_scenes.size()-1)
-	var newGrid = Grid_scenes[randomGridInt].instantiate()
-	add_child(newGrid)
-	global_position = global_position - Vector3(0,2,0)
+	var gridmap : GridMap = GridMap.new()
+	add_child(gridmap)
+	gridmap.set_cell_size(Vector3(1,1,1))
+	gridmap.set_cell_scale(1)
+	gridmap.set_center_x(true)
+	gridmap.set_center_y(true)
+	gridmap.set_center_z(true)
+	gridmap.set_mesh_library(mesh_lib_src)
+	gridmap.name = "BaseGrid"
+	
+	gridmap.global_position = gridmap.global_position - Vector3(0,1,0)
+	
+	for x in range(-preGenSize, preGenSize+1):
+		for z in range(-preGenSize, preGenSize+1):
+			var grass_pos = Vector3i(x,1,z)
+			var dirt_pos = Vector3i(x,0,z)
+			gridmap.set_cell_item(grass_pos, 0)
+			gridmap.set_cell_item(dirt_pos, 8)
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
