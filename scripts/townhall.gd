3963fb985ed89550134a75f92cce776dbaab9176
timmno12 <benjamintimm21@gmail.com>
2024-01-23 09:45:08 +0100

    :/

---
+++ scripts/townhall.gd
@@ -0,0 +1,67 @@
+extends Node3D
+
+@onready var citizen=ResourceLoader.load("res://scenes/game_scenes/citizen.tscn")
+@onready var check_for_tree_and_bush = $CheckForTreeAndBush
+@onready var navigation_region_3d = $".."
+@onready var audio_stream_player = $AudioStreamPlayer3D
+
+
+var spawn_ready:=false
+var spawn_timer:=0.0
+var spawn_interval:=3.0
+var current_citizen
+var current_pop:=0
+var max_pop:= 10
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func run_spawn():
+	current_citizen=citizen.instantiate()
+	
+	
+	get_tree().root.add_child(current_citizen,true)
+	current_citizen.spawn_point=$SpawnPoint
+	current_citizen.global_position=$SpawnPoint.global_position
+	current_pop+=1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if spawn_ready and current_pop<max_pop:
+		spawn_timer += delta
+		if spawn_timer >= spawn_interval:
+			spawn_timer = 0  
+			run_spawn()
+
+		
+		
+func _enter_tree():
+	spawn_ready=true
+
+
+func _on_check_for_tree_and_bush_body_entered(body):
+	if body.is_in_group("food"):
+		GameManager.bush_array.erase(body)
+		body.queue_free()
+		print(str(body) + " removed")
+		
+	if body.is_in_group("wood"):
+		GameManager.tree_array.erase(body)
+		body.queue_free()
+		print(str(body) + " removed")
+		
+	
+
+
+func _on_random_events_sabotage_started():
+	audio_stream_player.play(0)
+
+
+func _on_fire_sabotage_stopped():
+	audio_stream_player.stop()
+
+
+func _on_tornado_sabotage_stopped():
+	audio_stream_player.stop()
